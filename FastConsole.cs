8b1e3cfe6c056800db159cb2b4480f62bf27f6d7
TEMARI VÍRUS <103554944+Zemogus@users.noreply.github.com>
2022-08-19 01:01:42 +0800

    Added user input functionality and callbacks

--- FastConsole.cs
+++ FastConsole.cs
@@ -1,11 +1,48 @@
 using Microsoft.Win32.SafeHandles;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading;
+using System.Windows.Input;
 
 static class FastConsole
 {
+    static SafeFileHandle ConoutHandle;
+
+    private static Stopwatch Time = Stopwatch.StartNew();
+    public static Thread RenderThread { get; private set; }
+    public static Thread InputThread { get; private set; }
+    static Action RenderCallback;
+    static Action InputLoopCallback;
+
+    // Throws an exception if it failed to grab the CONOUT$ file handle
+    // Otherwise, starts the console update loop on a separate thread
+    // The callback is called at the end of each frame
+    public static void Initialise(Action renderCallback = null, Action inputCallback = null)
+    {
+        ConoutHandle = CreateFile("CONOUT$", 0x40000000, 2, IntPtr.Zero, FileMode.Open, 0, IntPtr.Zero);
+
+        if (ConoutHandle.IsInvalid) throw new System.ComponentModel.Win32Exception();
+        else
+        {
+            Width = (short)Console.BufferWidth;
+            Height = (short)Console.BufferHeight;
+            Console.CursorVisible = CursorVisible;
+            ConsoleBuffer = new int[Width * Height];
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            RenderThread = new Thread(RenderLoop);
+            RenderCallback = renderCallback;
+            RenderThread.Start();
+
+            InputThread = new Thread(InputLoop);
+            InputThread.SetApartmentState(ApartmentState.STA);
+            InputLoopCallback = inputCallback;
+            InputThread.Start();
+        }
+    }
+   
     #region // Font
     private const int FixedWidthTrueType = 54;
     private const int StandardOutputHandle = -11;
@@ -38,7 +75,7 @@ static class FastConsole
         public string FontName;
     }
 
-    public static FontInfo[] SetCurrentFont(string font, short fontSize = 0)
+    public static FontInfo[] SetFont(string font, short fontSize = 0)
     {
         FontInfo before = new FontInfo
         {
@@ -83,13 +120,26 @@ static class FastConsole
     }
     #endregion
 
+    #region // Rendering
     private static int Width, Height;
     private static int XOffset, YOffset;
-    public static bool CursorVisible = false;
+    private static bool _CursorVisible = true;
+    public static bool CursorVisible
+    {
+        get => _CursorVisible;
+        set
+        {
+            if (_CursorVisible != value)
+            {
+                _CursorVisible = value;
+                Console.CursorVisible = value;
+            }
+        }
+    }
     public static int CursorLeft = 0, CursorTop = 0;
-    public static float Framerate;
+    public static double Framerate = 30;
+
     static int[] ConsoleBuffer;
-    static SafeFileHandle ConoutHandle;
 
     [DllImport("Kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
     static extern SafeFileHandle CreateFile(
@@ -140,26 +190,13 @@ static class FastConsole
         }
     }
 
-    // Returns false if it failed to grab the CONOUT$ file handle
-    // Otherwise, returns true
-    public static bool Initialise()
+    private static void RenderLoop()
     {
-        Width = (short)Console.BufferWidth;
-        Height = (short)Console.BufferHeight;
-
-        ConsoleBuffer = new int[Width * Height];
-        Console.OutputEncoding = System.Text.Encoding.Unicode;
-
-        ConoutHandle = CreateFile("CONOUT$", 0x40000000, 2, IntPtr.Zero, FileMode.Open, 0, IntPtr.Zero);
-        return !ConoutHandle.IsInvalid;
-    }
-
-    public static void ConsoleUpdateLoop()
-    {
-        if (!Initialise()) throw new Exception("Failed to initialise console!");
-
+        Queue<long> times = new Queue<long>();
+        long prevT = 0;
         while (true)
         {
+            // Handle window being resized
             try
             {
                 Console.BufferWidth = Math.Max(Width, Console.WindowWidth);
@@ -169,7 +206,6 @@ static class FastConsole
             int old_xoffset = XOffset, old_yoffset = YOffset;
             XOffset = (Console.WindowWidth - Width) / 2;
             YOffset = (Console.WindowHeight - Height) / 2;
-            Console.CursorVisible = CursorVisible;
             if (old_xoffset != XOffset || old_yoffset != YOffset)
             {
                 // Fill the whole buffer with black
@@ -178,9 +214,29 @@ static class FastConsole
                 Rect void_rect = new Rect(0, 0, width, height);
                 WriteConsoleOutputW(ConoutHandle, the_void, new Coord((short)width, (short)height), Coord.Zero, ref void_rect);
             }
-            Render();
-            Thread.Sleep((int)(1000 / Framerate));
+            ForceRender();
+
+            // Write fps
+            long newT = Time.ElapsedTicks;
+            times.Enqueue(newT);
+            long oldT;
+            if (times.Count > Framerate) oldT = times.Dequeue();
+            else oldT = times.Peek();
+            WriteAt($"{(float)(times.Count - 1) / (newT - oldT) * Stopwatch.Frequency}fps".PadRight(12), 0, 0);
+
+            // Callback
+            RenderCallback?.Invoke();
+
+            // Wait until next render
+            Thread.Sleep((int)Math.Max(0, 0.9 * (1000D / Framerate) - (1000D * (Time.ElapsedTicks - prevT) / Stopwatch.Frequency)));
+            //while (Time.ElapsedTicks - prevT < Stopwatch.Frequency / Framerate * 0.95) Thread.Sleep(0);
+            prevT = Time.ElapsedTicks;
         }
+    }
+
+    public static void SetRenderCallback(Action callback)
+    {
+        RenderCallback = callback;
     }
 
     public static void SetWindow(int width, int height)
@@ -191,21 +247,34 @@ static class FastConsole
 
     public static void SetBuffer(int width, int height)
     {
-        Console.BufferWidth = width;
-        Console.BufferHeight = height;
-        ResizeBuffer(width, height);
+        if (width != Width) Console.BufferWidth = width;
+        if (height != Height) Console.BufferHeight = height;
+        if (width != Width || height != Height) ResizeBuffer(width, height);
     }
 
     public static void Set(int window_width, int window_height, int buffer_width, int buffer_height)
     {
-        if (window_width <= Console.BufferWidth) Console.WindowWidth = window_width;
-        Console.BufferWidth = buffer_width;
-        Console.WindowWidth = window_width;
-        if (window_height <= Console.BufferHeight) Console.WindowHeight = window_height;
-        Console.BufferHeight = buffer_height;
-        Console.WindowHeight = window_height;
+        try
+        {
+            if (window_width <= Console.BufferWidth) Console.WindowWidth = window_width;
+        } catch { }
+        if (buffer_width != Width) Console.BufferWidth = buffer_width;
+        try
+        {
+            Console.WindowWidth = window_width;
+        } catch {}
+        try
+        {
+            if (window_height <= Console.BufferHeight) Console.WindowHeight = window_height;
+        } catch { }
+        if (buffer_height != Height) Console.BufferHeight = buffer_height;
+        try
+        {
+            Console.WindowHeight = window_height;
+        } catch { }
 
-        ResizeBuffer(buffer_width, buffer_height);
+        if (buffer_width != Width || buffer_height != Height)
+            ResizeBuffer(buffer_width, buffer_height);
     }
 
     static void ResizeBuffer(int width, int height)
@@ -251,9 +320,152 @@ static class FastConsole
         ConsoleBuffer = new int[Width * Height];
     }
 
-    static void Render()
+    public static void ForceRender()
     {
         Rect rect = new Rect(XOffset, YOffset, XOffset + Width, YOffset + Height);
         WriteConsoleOutputW(ConoutHandle, ConsoleBuffer, new Coord(Width, Height), Coord.Zero, ref rect);
     }
+    #endregion
+
+    #region // User Input
+    private class KeyListener
+    {
+        public readonly Key Key;
+        public readonly bool OnPress;
+        public readonly int Delay, RepeatDelay;
+        public readonly Action Action;
+
+        public bool LastIsPressed;
+        public long LastStateChangeTime;
+
+        public bool Remove = false;
+
+        public KeyListener(Key key, bool onPress, int delayInMilliseconds, int repeatDelayInMilliseconds, Action action)
+        {
+            Key = key;
+            OnPress = onPress;
+            Delay = delayInMilliseconds;
+            RepeatDelay = repeatDelayInMilliseconds;
+            Action = action;
+            Thread getIsDown = new Thread(() => LastIsPressed = Keyboard.IsKeyDown(key));
+            getIsDown.SetApartmentState(ApartmentState.STA);
+            getIsDown.Start();
+            getIsDown.Join();
+            LastStateChangeTime = Time.ElapsedMilliseconds;
+        }
+
+        public void Check()
+        {
+            bool isPressed = Keyboard.IsKeyDown(Key);
+            long currentTime = Time.ElapsedMilliseconds;
+
+            // If key changed state
+            if (LastIsPressed != isPressed)
+            {
+                LastIsPressed = isPressed;
+                LastStateChangeTime = currentTime;
+
+                if (isPressed == OnPress)
+                {
+                    if (Delay <= 0)
+                        Action?.Invoke();
+                    if (Delay == 0)
+                        LastStateChangeTime += RepeatDelay; // - Delay, but Delay == 0
+                }
+            }
+            else if (Delay >= 0)
+            {
+                if (isPressed == OnPress)
+                {
+                    if (currentTime - LastStateChangeTime > Delay)
+                    {
+                        Action?.Invoke();
+                        LastStateChangeTime = currentTime + RepeatDelay - Delay;
+                        if (RepeatDelay < 0) LastStateChangeTime = long.MinValue;
+                    }
+                }
+            }
+        }
+    }
+
+    private static readonly List<KeyListener> KeyListeners = new List<KeyListener>();
+
+    private static void InputLoop()
+    {
+        Queue<long> times = new Queue<long>();
+        while (true)
+        {
+            long time = Time.ElapsedTicks;
+            // Remove listeners that are to be removed
+            for (int i = 0; i < KeyListeners.Count; i++)
+                if (KeyListeners[i] != null)
+                    if (KeyListeners[i].Remove)
+                        KeyListeners.RemoveAt(i--);
+            // Check for key events
+            for (int i = 0; i < KeyListeners.Count; i++)
+                KeyListeners[i]?.Check();
+
+            // Callback function
+            InputLoopCallback?.Invoke();
+
+            // Try to get ~2000 loops per second, since the smallest precision is a millisecond
+            while (Time.ElapsedTicks - time < Stopwatch.Frequency / 2400) Thread.Sleep(0);
+        }
+    }
+
+    public static void SetInputLoopCallback(Action callback)
+    {
+        InputLoopCallback = callback;
+    }
+
+    private static void AddListener(Key key, bool onPress, int delayInMilliseconds, int repeatDelayInMilliseconds, Action action) =>
+        KeyListeners.Add(new KeyListener(key, onPress, delayInMilliseconds, repeatDelayInMilliseconds, action));
+
+    public static void AddOnPressListener(Key key, Action action) =>
+        AddListener(key, true, -1, 0, action);
+
+    public static void AddOnReleaseListener(Key key, Action action) =>
+        AddListener(key, false, -1, 0, action);
+
+    public static void AddOnHoldListener(Key key, Action action, int delayInMilliseconds, int repeatDelayInMilliseconds) =>
+        AddListener(key, true, delayInMilliseconds, repeatDelayInMilliseconds, action);
+
+    private static void RemoveListeners(Key key, bool onPress, bool onHold)
+    {
+        for (int i = 0; i < KeyListeners.Count; i++)
+        {
+            if (KeyListeners[i].Remove) continue;
+
+            if (KeyListeners[i].Key == key && KeyListeners[i].OnPress == onPress)
+            {
+                //if (onHold)
+                //{
+                //    if (KeyListeners[i].Delay >= 0)
+                //        KeyListeners[i].Remove = true;
+                //}
+                //else if (KeyListeners[i].Delay < 0)
+                //    KeyListeners[i].Remove = true;
+
+                // Same as above
+                KeyListeners[i].Remove = onHold ^ (KeyListeners[i].Delay < 0);
+            }
+        }
+    }
+
+    public static void RemoveAllListeners(Key key)
+    {
+        for (int i = 0; i < KeyListeners.Count; i++)
+            if (KeyListeners[i].Key == key)
+                KeyListeners[i].Remove = true;
+    }
+
+    public static void RemoveOnPressListeners(Key key) =>
+        RemoveListeners(key, true, false);
+
+    public static void RemoveOnReleaseListeners(Key key) =>
+        RemoveListeners(key, false, false);
+
+    public static void RemoveOnHoldListeners(Key key) =>
+        RemoveListeners(key, true, true);
+    #endregion
 }
